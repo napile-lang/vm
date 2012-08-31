8dcd6ec5ae1c5af97430986f65ad3dfd38980da2
macournoyer <macournoyer@gmail.com>
2012-08-31 11:22:10 -0400

    Implement VM instructions.

--- vm.c
+++ vm.c
@@ -27,7 +27,79 @@ void run(void *literals[], byte instructions[]) {
   // Start processing instructions
   while (1) {
     switch (*ip) {
-      
+      case CALL: {
+        ip++; // advance to operand (method)
+        char *method = (char *)literals[*ip];
+        ip++; // # of arguments
+        int argc = *ip;
+        
+        Object *argv[10];
+        int i;
+        for(i = 0; i < argc; ++i) argv[i] = STACK_POP();
+        Object *receiver = STACK_POP();
+        
+        Object *result = call(receiver, method, argv, argc);
+        STACK_PUSH(result);
+        
+        break;
+      }
+      case PUSH_NUMBER: {
+        ip++; // advance to operand (literal index)
+        STACK_PUSH(Number_new((long)literals[*ip]));
+        break;
+      }
+      case PUSH_STRING: {
+        ip++; // advance to operand (literal index)
+        STACK_PUSH(String_new((char *)literals[*ip]));
+        break;
+      }
+      case PUSH_SELF: {
+        STACK_PUSH(self);
+        break;
+      }
+      case PUSH_NIL: {
+        STACK_PUSH(NilObject);
+        break;
+      }
+      case PUSH_BOOL: {
+        ip++;
+        if (*ip == 0) {
+          STACK_PUSH(FalseObject);
+        } else {
+          STACK_PUSH(TrueObject);
+        }
+        break;
+      }
+      case GET_LOCAL: {
+        ip++;
+        STACK_PUSH(locals[*ip]);
+        break;
+      }
+      case SET_LOCAL: {
+        ip++;
+        locals[*ip] = STACK_POP();
+        break;
+      }
+      case ADD: {
+        Object *a = STACK_POP();
+        Object *b = STACK_POP();
+        
+        STACK_PUSH(Number_new(Number_value(a) + Number_value(b)));
+        
+        break;
+      }
+      case JUMP_UNLESS: {
+        ip++; // offset
+        byte offset = *ip;
+        Object *condition = STACK_POP();
+        
+        if (!Object_is_true(condition)) ip += offset;
+        
+        break;
+      }
+      case RETURN: {
+        return;
+      }
     }
     ip++;
   }
