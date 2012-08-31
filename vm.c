71b8b3ff4f38ff4f53046e457ee60febbeaeffeb
macournoyer <macournoyer@gmail.com>
2012-08-31 12:23:15 -0400

    Implement a refcount GC.

--- vm.c
+++ vm.c
@@ -9,7 +9,7 @@
 #define STACK_PUSH(I)  do {                             \
                           assert(sp-stack < STACK_MAX); \
                           *(++sp) = (I);                \
-                       } while(0)
+                          retain(*sp); } while(0)
 #define STACK_POP()    (*sp--)
 
 
@@ -23,6 +23,7 @@ void run(void *literals[], byte instructions[]) {
   
   // Setup the runtime
   Object *self = Object_new();
+  retain(self);
   
   // Start processing instructions
   while (1) {
@@ -40,6 +41,10 @@ void run(void *literals[], byte instructions[]) {
         
         Object *result = call(receiver, method, argv, argc);
         STACK_PUSH(result);
+        
+        // Release object
+        release(receiver);
+        for(i = 0; i < argc; ++i) release(argv[i]);
         
         break;
       }
@@ -86,6 +91,9 @@ void run(void *literals[], byte instructions[]) {
         
         STACK_PUSH(Number_new(Number_value(a) + Number_value(b)));
         
+        release(a);
+        release(b);
+        
         break;
       }
       case JUMP_UNLESS: {
@@ -94,6 +102,8 @@ void run(void *literals[], byte instructions[]) {
         Object *condition = STACK_POP();
         
         if (!Object_is_true(condition)) ip += offset;
+        
+        release(condition);
         
         break;
       }
@@ -106,11 +116,18 @@ void run(void *literals[], byte instructions[]) {
         break;
       }
       case RETURN: {
-        return;
+        goto cleanup;
+        break;
       }
     }
     ip++;
   }
+
+cleanup:
+  release(self);
+  while (sp > stack) release(STACK_POP());
+  int i;
+  for(i = 0; i < 10; ++i) if (locals[i]) release(locals[i]);
 }
 
 int main (int argc, char const *argv[]) {
